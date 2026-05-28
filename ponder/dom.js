d5b32dd08d369a8d5d6ba2884ee3e67ca9c7a5e9
Tobi-013 <oluwaiyetan@gmail.com>
2026-05-28 16:23:09 -0600

    .

--- ponder/dom.js
+++ ponder/dom.js
@@ -28,6 +28,6 @@ let selectElem = document.getElementById('webdevlist');
 selectElem.addEventListener('change', function(){
     let codeValue = selectElem.value;
     console.log(codeValue)
-    document.getElementById(codeValue).style,color = 'red';
+    document.getElementById(codeValue).style.color = 'red';
 })
                 
