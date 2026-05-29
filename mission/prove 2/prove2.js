961b6b0fdb493a8de0f3bd950c67edafb46bf1f7
Tobi-013 <oluwaiyetan@gmail.com>
2026-05-29 00:50:09 -0600

    DOM Manipulation

---
+++ mission/prove 2/prove2.js
@@ -0,0 +1,22 @@
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+
+    let current = selectElem.value;
+
+    if (current == 'dark') {
+
+        document.body.classList.add('dark');
+
+        logo.src = "images/byui-logo-dark.png";
+
+    } else {
+
+        document.body.classList.remove('dark');
+
+        logo.src = "../images/byui-logo-blue.png";
+    }
+}
