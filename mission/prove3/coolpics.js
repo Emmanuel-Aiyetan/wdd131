04c7f9a86f251f0056b543d3ee5e20fd01a041be
Tobi-013 <oluwaiyetan@gmail.com>
2026-06-02 17:31:35 -0600

    Cool Pics part 2

---
+++ mission/prove3/coolpics.js
@@ -0,0 +1,41 @@
+const btn = document.querySelector('.menu-btn');
+const menu = document.querySelector('nav');
+
+btn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    menu.classList.toggle('show');
+}
+
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+gallery.addEventListener('click', openModal);
+
+function openModal(event) {
+
+    const img = event.target;
+
+    const src = img.getAttribute('src');
+    const alt = img.getAttribute('alt');
+
+    const full = src.replace('-sm', '-full');
+
+    modalImage.src = full;
+    modalImage.alt = alt;
+
+    modal.showModal();
+}
+
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+modal.addEventListener('click', (event) => {
+
+    if (event.target === modal) {
+        modal.close();
+    }
+
+});
